bee0cede6cd0d4a5d846f06bfc9a6df84ded7249
Peter Takacs <peter.takacs@koodilab.com>
2025-02-14 22:59:58 +0100

    Added header constants

--- middleware.go
+++ middleware.go
@@ -5,14 +5,14 @@ import "net/http"
 // Middleware function.
 func (i *Inertia) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("X-Inertia") == "" {
+		if r.Header.Get(HeaderInertia) == "" {
 			next.ServeHTTP(w, r)
 
 			return
 		}
 
-		if r.Method == "GET" && r.Header.Get("X-Inertia-Version") != i.version {
-			w.Header().Set("X-Inertia-Location", i.url+r.RequestURI)
+		if r.Method == "GET" && r.Header.Get(HeaderVersion) != i.version {
+			w.Header().Set(HeaderLocation, i.url+r.RequestURI)
 			w.WriteHeader(http.StatusConflict)
 
 			return
